fb61efd2398630f778d7f1b4e0e5a376191597d7
ce-ta <asahitsuchida7865@gmail.com>
2026-04-22 21:22:13 +0900

    first commit

---
+++ frontend/src/components/MapView.tsx
@@ -0,0 +1,41 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+
+const nurseries = [
+  {
+    id: 1,
+    name: "駒場保育園",
+    lat: 35.6565735,
+    lng: 139.6867123,
+  },
+  {
+    id: 2,
+    name: "菅刈保育園",
+    lat: 35.65018267,
+    lng: 139.695026,
+  },
+]
+
+export const MapView = () => {
+  return (
+    <MapContainer
+      center={[35.4478, 139.6425]}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {/* ピン表示 */}
+      {nurseries.map((n) => (
+        <Marker key={n.id} position={[n.lat, n.lng]}>
+          <Popup>
+            <div>
+              <h3>{n.name}</h3>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  )
+}
---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import 'leaflet/dist/leaflet.css'
+import { MapView } from './components/MapView.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MapView />
+  </StrictMode>,
+)
