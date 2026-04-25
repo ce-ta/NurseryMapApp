6da2469f0e5abab0a9d69f349f03f07f9a9b611d
ce-ta <asahitsuchida7865@gmail.com>
2026-04-25 15:34:41 +0900

    ピンをクリックしたら詳細情報が表示される

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './main.css'
 import 'leaflet/dist/leaflet.css'
 import { MapView } from './components/MapView.tsx'
 
